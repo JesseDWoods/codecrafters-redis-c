1b3a2d252d84eff4782527fbfa1fe2a12bdd0e79
Jesse <jessedavidwoods@outlook.com>
2026-04-22 14:20:02 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -145,7 +145,7 @@ int main() {
     return EXIT_SUCCESS;
 }
 void process_input(ClientConnection * client, RESP_list *list, struct hashMap *key_value) {
-    printf("Processing input with key_value: %p\n", key_value);
+    printf("Processing");
 
     while (list != NULL && list->head != NULL) {
 
